4fc43c54b0276cd2423d43c1f0112db6d7a0f483
Manfrin <matheus_mm123@hotmail.com>
2022-05-10 23:27:00 -0300

    feat: add my credits to project

--- app/src/styles/DefaultLayout/index.tsx
+++ app/src/styles/DefaultLayout/index.tsx
@@ -3,30 +3,22 @@ interface IDefaultLayoutProps {
 }
 
 export default function DefaultLayout({ children }: IDefaultLayoutProps) {
-  return <div className="m-auto py-2 px-4 max-w-3xl">{children}</div>;
+  return (
+    <div className="flex flex-col m-auto py-2 px-4 max-w-3xl h-full">
+      {children}
+
+      <footer className="mt-auto py-2 ">
+        <p>
+          Made By Matheus Manfrin -{" "}
+          <a
+            href="https://api.whatsapp.com/send?phone=556496140384&text=Ol%C3%A1,%20vim%20pelo%20MYTC."
+            target="_blank"
+            rel="noreferrer"
+          >
+            (64) 9 9614-0384
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
-
-// export const Container = styled.div`
-//   display: flex;
-//   flex-direction: row;
-
-//   margin: 16px 16px 8px;
-//   border-radius: 32px;
-//   padding: 8px 16px;
-//   box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2);
-
-//   background: #3b9974;
-//   min-height: 95vh;
-
-//   color: #fff;
-// `;
-
-// export const Wrapper = styled.section`
-//   border-radius: 32px;
-//   width: 100%;
-//   /* height: 100%; */
-//   padding: 16px 8px;
-//   background: #fdfeff;
-//   margin-left: 16px;
-//   color: #000;
-// `;
